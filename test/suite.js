720174704133dbb63fe36ef11dc790083e86c2a3
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-05-30 13:38:55 +0200

    Implement first test case

--- test/suite.js
+++ test/suite.js
@@ -1,12 +1,16 @@
 Upload = require('../lib/');
+assert = require('assert');
+S3 = require('aws-sdk').S3;
 
 client = null
 
 beforeEach(function() {
-  client = new Upload();
+  client = new Upload('turadmin', {path: 'images/'});
 });
 
 describe('new Client()', function() {
-  it('should do this')
+  it('should instasiate correctly', function() {
+    assert(client.s3 instanceof S3);
+  });
 });
 
